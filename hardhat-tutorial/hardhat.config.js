f849fef43b50023f109e37cefea2fd4840dbcd2d
Aditya Agarwal <adityag2511@gmail.com>
2022-12-12 05:12:45 -0500

    Added hardhat deployment scripts

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const AVAXFUJI_RPC_URL = process.env.AVAXFUJI_RPC_URL;
+
+const AVAXFUJI_PRIVATE_KEY = process.env.AVAXFUJI_PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    avax_fuji: {
+      url: AVAXFUJI_RPC_URL,
+      accounts: [AVAXFUJI_PRIVATE_KEY],
+    },
+  },
 };
