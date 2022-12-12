1061704cd19035449bd0887d244bd320eb125b6a
Aditya Agarwal <adityag2511@gmail.com>
2022-12-12 04:10:16 -0500

    Added Vot and Proposal Structs

--- hardhat-tutorial/contracts/AnyChainDAO.sol
+++ hardhat-tutorial/contracts/AnyChainDAO.sol
@@ -6,5 +6,28 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 // We will add the Interfaces here
 
 contract AnyChainDAO is Ownable {
-    // We will write contract code here
+    
+    // Create a struct named votes to store counts for a proposal
+    struct Vote {
+        // inFavor - number of yes votes for this proposal
+        uint256 inFavor;
+        // against - number of no votes for this proposal
+        uint256 against;
+        // abstain - number of neutral votes for this proposal
+        uint256 abstain;
+    }
+
+    // Create a struct named Proposal containing all relevant information
+    struct Proposal {
+        // proposalTitle - The purpose of the proposal (Here represents the action to take afterwards)
+        string proposalTitle;
+        // deadline - the UNIX timestamp until which this proposal is active. Proposal can be executed after the deadline has been exceeded.
+        uint256 deadline;
+        // votes - Count of different votes cast on-chain for the proposal
+        Vote votes;
+        // executed - whether or not this proposal has been executed yet. Cannot be executed before the deadline has been exceeded.
+        bool executed;
+        // voters - a mapping of CryptoDevsNFT tokenIDs to booleans indicating whether that NFT has already been used to cast a vote or not
+        mapping(uint256 => bool) voters;
+    }
 }
