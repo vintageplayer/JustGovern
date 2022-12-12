2dcdde7da5bad2cffc7f103bd186b3acaff7e439
Aditya Agarwal <adityag2511@gmail.com>
2022-12-12 03:56:17 -0500

    Added Contract Template

---
+++ hardhat-tutorial/contracts/AnyChainDAO.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// We will add the Interfaces here
+
+contract AnyChainDAO is Ownable {
+    // We will write contract code here
+}
